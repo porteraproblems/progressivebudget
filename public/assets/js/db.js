f3bc8d5bf1820b2bed3c8e528a7b1a755f4fbf7a
Alexander Portera <aportera3@gmail.com>
2020-12-28 12:30:09 -0500

    commit

--- public/assets/js/db.js
+++ public/assets/js/db.js
@@ -7,3 +7,16 @@ request.onupgreadeneeded = function (event) {
     const db = event.target.result;
     db.createObjectStore("pending", { autoIncrement: true });
 };
+
+request.onsuccess = function (event) {
+    db = event.target.request;
+
+
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+request.onerror = function (event) {
+    console.log("Error: " + event.target.errorCode);
+};
