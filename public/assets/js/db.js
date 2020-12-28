ad1852c08eaa42e54c1c244610957c0550b6f420
Alexander Portera <aportera3@gmail.com>
2020-12-28 13:13:32 -0500

    commit

--- public/assets/js/db.js
+++ public/assets/js/db.js
@@ -3,15 +3,12 @@ let db;
 const request = indexedDB.open("budget", 1);
 
 request.onupgreadeneeded = function (event) {
-
     const db = event.target.result;
     db.createObjectStore("pending", { autoIncrement: true });
 };
 
 request.onsuccess = function (event) {
     db = event.target.request;
-
-
     if (navigator.onLine) {
         checkDatabase();
     }
@@ -20,3 +17,38 @@ request.onsuccess = function (event) {
 request.onerror = function (event) {
     console.log("Error: " + event.target.errorCode);
 };
+
+function saveRecord(record) {
+
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.add(record);
+}
+
+function checkDatabase() {
+
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getALL();
+
+    getAll.onsuccess = function () {
+        if (getAll.request.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                const transaction = db.transaction(["pending"], "readwrite");
+                const store = transaction.objectStore("pending");
+                store.clear();
+            });
+        }
+    };
+}
+
+window.addEventListener("online", checkDatabase);
